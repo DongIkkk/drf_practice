700cca7bddfc22ab78324835f272424fb77c6ac7
DongIkkk <w8h0412@gmail.com>
2022-10-25 14:47:43 +0900

    serializer updata, article API

--- articles/urls.py
+++ articles/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from articles import views
 
 urlpatterns = [
-    path('index/', views.index, name='index'),
+    path('', views.articleAPI, name='index'),
+    path('<int:article_id>/', views.articleDeatailAPI, name='article_view'),
 ]
